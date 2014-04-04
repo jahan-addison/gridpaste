458a097ab8d60895605e3a5b09a7f0412d9453da
Jahan Addison <jahan.addison@jacata.me>
2014-04-04 16:04:11 -0400

    keep aspect ratio. closes #22

--- public/js/main.js
+++ public/js/main.js
@@ -6,8 +6,12 @@ $(function() {
     /* Board Options */
     JXG.Options.angle.orthoType = "root";
     JXG.Options.angle.radius    = 25;
-    
-    board     = JXG.JSXGraph.initBoard('grid', {boundingbox:[-100,60,100,-60]});
+    board     = JXG.JSXGraph.initBoard('grid', {
+      boundingbox:     [-100,60,100,-60],
+      keepaspectratio: true,
+      showCopyright:   false,
+      showNavigation:  false 
+    });
     var axx   = board.create('axis',[[0,0],[1,0]]);
     var axy   = board.create('axis',[[0,0],[0,1]]);
      
