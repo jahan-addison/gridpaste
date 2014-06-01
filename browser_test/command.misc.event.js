4bf4543b480006eb3b64792c029f8ec68bed84c4
Jahan Addison <jahan.addison@jacata.me>
2014-05-31 21:35:09 -0400

    misc Command concrete test suite

--- browser_test/command.misc.event.js
+++ browser_test/command.misc.event.js
@@ -1,10 +1,10 @@
 var draw = require('../public/app/events/draw'),
     misc = require('../public/app/events/misc');
 
-describe("msic Command concrete", function() {
-  var brd, command;
+describe("misc Command concrete", function() {
+  var brd, command, circle;
   before(function(done) {
-    document.body.innerHTML = window.__html__['browser_test/fixtures/draw.html'];
+    document.body.innerHTML = window.__html__['browser_test/fixtures/delete.html'];
     brd = JXG.JSXGraph.initBoard('grid', {
       boundingbox:     [-50, 50, 50, -50],
       keepaspectratio: true,
@@ -16,15 +16,33 @@ describe("msic Command concrete", function() {
     brd.shapes = [];
     done();
   });
+  beforeEach(function() {
+    circle  = new draw.circle(brd);
+    circle.execute();
+  });
   afterEach(function() {
     try { command.remove(); } catch(e) {}
+    circle.remove();
   });
   describe("misc.delete_ command", function() {
-    it("should receive arguments from DOM if not provided and delete a figure", function() {
+    it("should receive arguments from DOM if not provided and 'delete' a figure", function() {
+      command = new misc.delete_(brd);
+      command.execute();
+      brd.shapes[0].isVisible.should.be.false;
     });
-    it("should use passed arguments if provided and delete a figure", function() {
+    it("should use passed arguments if provided and 'delete' a figure", function() {
+      command = new misc.delete_(brd, {
+        figure: 'A1'}
+      );
+      command.execute();
+      brd.shapes[0].isVisible.should.be.false;
     });
     it("can be undone", function() {
+      command = new misc.delete_(brd);
+      command.execute();
+      brd.shapes[0].isVisible.should.be.false;
+      command.remove();
+      brd.shapes[0].isVisible.should.be.true;
     });
   });
 });
