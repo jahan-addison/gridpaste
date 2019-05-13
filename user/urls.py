e72c7f2a4f75015ccd975be0b44d659de009b121
bramz <brockramz@gmail.com>
2019-05-12 23:56:16 -0400

    Add user login and registration, resolves #107, #111, #112

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,24 @@
-from django.urls import path
+from django.urls import include, path
+from django.conf.urls import url
 
 from . import views
 
+
+""" 
+This will cover the following paths with djangos built in authentication system.
+user/login/ [name='login']
+user/logout/ [name='logout']
+user/password_change/ [name='password_change']
+user/password_change/done/ [name='password_change_done']
+user/password_reset/ [name='password_reset']
+user/password_reset/done/ [name='password_reset_done']
+user/reset/<uidb64>/<token>/ [name='password_reset_confirm']
+user/reset/done/ [name='password_reset_complete']
+user/register
+"""
+
 urlpatterns = [
-    path('login/', views.login),
-    path('logout/', views.logout),
-    path('register/', views.register),
+    url(r'^register/$', views.register, name='register'),
+    path('', include('django.contrib.auth.urls')),
+
 ]
