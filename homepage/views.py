ef00896572b88f26b2950c07230cf9073f636da0
bramz <brockramz@gmail.com>
2019-06-14 21:27:16 +0000

    Update examples view

--- homepage/views.py
+++ homepage/views.py
@@ -4,13 +4,12 @@ from django.shortcuts import render
 
 from pastes.models import Pastes
 
-
 def index(request):
     template = get_template('index.html')
     html = template.render()
     return HttpResponse(html)
 
-
 def examples(request):
-    p = Pastes.objects.filter(user='examples')
+    p = Pastes.objects.filter(user='3')
     return render(request, 'examples.html', {'pastes': p})
+
